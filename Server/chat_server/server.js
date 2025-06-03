be8f0c5ee87aa19cbaf24ba6d450c4adf4920792
Khushi Jain <107362355+khushijain21200@users.noreply.github.com>
2025-06-03 12:13:36 +0530

    Update server.js

--- Server/chat_server/server.js
+++ Server/chat_server/server.js
@@ -148,7 +148,9 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
   console.log("Server running on port 3000");
 });
 
